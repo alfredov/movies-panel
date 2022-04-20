a7175aaf0179e4a00876e30e2320d57ce89e76f8
Alfredo Villagomez <alfredo.villagomez.m@gmail.com>
2022-04-20 14:17:41 -0400

    tools(storybook): support added to typescript alias

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,18 @@
+const path = require('path')
+
 module.exports = {
+  // we need to add aliases to webpack so it knows how to follow
+  // to the source of the packages rather than the built version (dist)
+  webpackFinal: async (config) => ({
+    ...config,
+    resolve: {
+      ...config.resolve,
+      alias: {
+        ...config.resolve.alias,
+        ...convertTsConfigPathsToWebpackAliases(),
+      },
+    },
+  }),
   "stories": [
     "../packages/ui/**/*.stories.mdx",
     "../packages/ui/**/*.stories.@(js|jsx|ts|tsx)"
@@ -9,4 +23,15 @@ module.exports = {
     "@storybook/addon-interactions"
   ],
   "framework": "@storybook/react"
+}
+
+function convertTsConfigPathsToWebpackAliases() {
+  const rootDir = path.resolve(__dirname, '../');
+  const tsconfig = require('../tsconfig.json');
+  const tsconfigPaths = Object.entries(tsconfig.compilerOptions.paths);
+
+  return tsconfigPaths.reduce((aliases, [realPath, mappedPath]) => {
+    aliases[realPath] = path.join(rootDir, mappedPath[0]);
+    return aliases;
+  }, {});
 }
