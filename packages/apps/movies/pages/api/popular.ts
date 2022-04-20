f8c88ea84b7c7711e74f8b69ff22d0eb6fc23db2
Alfredo Villagomez <alfredo.villagomez.m@gmail.com>
2022-04-20 17:54:17 -0400

    feat(movies): api/popular endpoint enabled

---
+++ packages/apps/movies/pages/api/popular.ts
@@ -0,0 +1,27 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import axios from 'axios'
+import 'dotenv/config'
+
+type Data = {
+  name: string
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  let url = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=es`
+  if (req.query.page) {
+    url = `${url}&page=${req.query.page}`
+  }
+
+  axios
+    .get(url)
+    .then(response => {
+      res.status(200).json(response.data)
+    })
+    .catch(error => {
+      res.status(500).json(error)
+    })
+}
